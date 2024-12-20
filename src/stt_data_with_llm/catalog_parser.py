f8c568d28a3b4c98cc7c8eda326b390867091394
jim-gyas <jackandthebean1234@gmail.com>
2024-12-20 11:35:09 +0530

    added try exception case in catalog parser

--- src/stt_data_with_llm/catalog_parser.py
+++ src/stt_data_with_llm/catalog_parser.py
@@ -1,3 +1,6 @@
+import json
+import logging
+
 import requests
 
 
@@ -10,20 +13,26 @@ def parse_catalog(catalog_url):
     Returns:
         dict: A dictionary of dictionaries containing the parsed catalog data.
     """
+    try:
+        response = requests.get(catalog_url)
+        catalog_data = response.json()
+        parsed_items = {}
+        for catalog_item in catalog_data:
+            parsed_item = {
+                "sr_no": catalog_item.get("Sr.no"),
+                "id": catalog_item.get("ID"),
+                "audio_url": catalog_item.get("Audio URL"),
+                "audio_text": catalog_item.get("Audio Text"),
+                "speaker_name": catalog_item.get("Speaker Name"),
+                "news_channel": catalog_item.get("News Channel"),
+                "publishing_year": catalog_item.get("Publishing Year"),
+            }
+            parsed_items[catalog_item.get("ID")] = parsed_item
 
-    response = requests.get(catalog_url)
-    catalog_data = response.json()
-    parsed_items = {}
-    for catalog_item in catalog_data:
-        parsed_item = {
-            "sr_no": catalog_item.get("Sr.no"),
-            "id": catalog_item.get("ID"),
-            "audio_url": catalog_item.get("Audio URL"),
-            "audio_text": catalog_item.get("Audio Text"),
-            "speaker_name": catalog_item.get("Speaker Name"),
-            "news_channel": catalog_item.get("News Channel"),
-            "publishing_year": catalog_item.get("Publishing Year"),
-        }
-        parsed_items[catalog_item.get("ID")] = parsed_item
-
-    return parsed_items
+        return parsed_items
+    except requests.RequestException as e:
+        logging.error(f"Error fetching Catalog: {e}")
+        return {}
+    except json.JSONDecodeError as e:
+        logging.error(f"Error parsing Catalog JSON: {e}")
+        return {}
