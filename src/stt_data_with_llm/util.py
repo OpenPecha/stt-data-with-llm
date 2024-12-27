d31194406cdf65eed4273065454444fba0fb6420
jim-gyas <jackandthebean1234@gmail.com>
2024-12-27 16:43:46 +0530

    added audio parser

--- src/stt_data_with_llm/util.py
+++ src/stt_data_with_llm/util.py
@@ -1,6 +1,4 @@
 import logging
-import random
-import time
 from logging.handlers import RotatingFileHandler
 
 from stt_data_with_llm.config import BACKUP_COUNT, MAX_BYTES
@@ -27,9 +25,3 @@ def setup_logging(filename):
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
-
-
-def generate_unique_id():
-    timestamp = int(time.time() * 1000)  # Current time in milliseconds
-    random_num = random.randint(1000, 9999)  # Random 4-digit number
-    return f"{timestamp:010d}_{random_num:04d}"  # noqa: E231
